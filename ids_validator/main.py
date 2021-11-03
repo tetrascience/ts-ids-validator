e18dec496107df0cd3f27dc1efe35693ec075bf3
Vladimir Shteyn <vshteyn@tetrascience.com>
2021-11-03 14:33:07 -0400

    debug main.py script

--- ids_validator/main.py
+++ ids_validator/main.py
@@ -1,12 +1,11 @@
 import argparse
 import sys
-from typing import List
 
 from ids_validator.convention_versions import Conventions
 from ids_validator.ids_validator import validate_ids
 
 
-def run(args: List[str]) -> None:
+def run() -> None:
     parser = argparse.ArgumentParser(
         description="Validate IDS Artifacts"
     )
@@ -14,9 +13,10 @@ def run(args: List[str]) -> None:
     parser.add_argument("ids_dir", type=str, help="Path to the IDS folder")
     parser.add_argument("--version", type=str, required=False, default=None,
                         help=f"Supported Versions: {Conventions.values()}")
-    parsed_args = parser.parse_args(args)
+    cli_args = sys.argv[1:]
+    parsed_args = parser.parse_args(cli_args)
     validate_ids(parsed_args)
 
 
 if __name__ == '__main__':
-    run(sys.argv[1:])
+    run()
