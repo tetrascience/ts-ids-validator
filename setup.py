0b941395b5d301d5a4a3d883acb90e764af9dadb
tariksetia <setia.tarik@gmail.com>
2021-11-08 16:38:58 +0530

    DE-2824: updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = "ts-ids-validator"
-VERSION = "0.9.1"
+VERSION = "0.9.2"
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
