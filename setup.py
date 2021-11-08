b6a25d249742cfbb2ca712aba7f4661f8d36274a
tariksetia <setia.tarik@gmail.com>
2021-11-08 16:53:56 +0530

    DE-2824: updated manifest and version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = "ts-ids-validator"
-VERSION = "0.9.2"
+VERSION = "0.9.3"
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
