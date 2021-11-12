af11dbd439045dd2a15cd0fb8498c3aa24b82cc4
tariksetia <setia.tarik@gmail.com>
2021-11-12 18:39:21 +0530

    DE-2627: added test cases for path normalization

---
+++ __tests__/unit/generic/test_athena_path_normalization.py
@@ -0,0 +1,27 @@
+import pytest
+from ids_validator.checks import AthenaChecker
+
+
+test_cases = {
+    'ABC': "abc",
+    'Abc123': "abc123",
+    'Abc_aBc': "abc_abc",
+    'abC_123': "abc_123",
+    "_abc": "abc",
+    "_+aBc": "abc",
+    "___aBc": "abc",
+    "abc__123": "abc_123",
+    "abc___123": "abc_123",
+    "abc___123__abc": "abc_123_abc",
+    "@abc_123": "abc_123",
+    "<>.,';|@abc_123": "abc_123",
+    "abc@___\"123": "abc_123",
+    "abc::[]{}@#$%^&*()-=+!123": "abc_123",
+    "aBc_/_123_\\_abc": "abc_123_abc",
+}
+
+
+@pytest.mark.parametrize("input_,expected", test_cases.items())
+def test_name_normalization(input_, expected):
+    result = AthenaChecker.normalize_path_name(input_)
+    assert result == expected
