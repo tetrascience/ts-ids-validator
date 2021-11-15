5af691f26feb6d45d2e1021760b77fabd84dab0d
tariksetia <setia.tarik@gmail.com>
2021-11-15 16:11:18 +0530

    DE-2880: use current directory as default ids dir

--- ids_validator/__main__.py
+++ ids_validator/__main__.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         description="Validate IDS Artifacts"
     )
 
-    parser.add_argument("ids_dir", type=str, help="Path to the IDS folder")
+    parser.add_argument("--ids_dir", type=str, default=".", required=False, help="Path to the IDS folder")
     parser.add_argument("--version", type=str, required=False, default=None,
                         help=f"Supported Versions: {Conventions.values()}")
     args = parser.parse_args()
